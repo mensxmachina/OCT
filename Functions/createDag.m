18d92234ec59a7849beaf5bf41339b894915275d
Konstantina Biza <98031699+konstantb@users.noreply.github.com>
2022-03-14 21:44:29 +0200

    Update createDag.m

--- Functions/createDag.m
+++ Functions/createDag.m
@@ -1,9 +1,9 @@
-function InputDag=createDag(Nnodes,MaxEdges)
+function InputDag=createDag(Nnodes, MaxEdges)
 
     maxParents=round((MaxEdges/Nnodes)*2);
     Number_edges=0;
 
-    if MaxEdges>Nnodes*(Nnodes-1)/2
+    if MaxEdges>(Nnodes*(Nnodes-1)/2)
         error("Error: number of edges");
     end
 
